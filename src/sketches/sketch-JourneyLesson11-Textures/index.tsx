822ef3d84bb5ddb759113481011877e8cadfa3d8
“Isaac <isaac@isaacmason.com>
2022-06-02 19:30:32 +1000

    Add more to JourneyLesson11-Textures

--- src/sketches/sketch-JourneyLesson11-Textures/index.tsx
+++ src/sketches/sketch-JourneyLesson11-Textures/index.tsx
@@ -1,8 +1,37 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useTexture } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import { useEffect } from 'react'
+import doorImage from './textures/door/color.jpg'
+import minecraftImage from './textures/minecraft.png'
+
+import * as THREE from 'three'
+
+// todo - https://threejs-journey.com/lessons/11#
+// up to ~24m
 
 const App = () => {
-    return null
+    const doorTexture = useTexture(doorImage)
+    doorTexture.wrapS = THREE.MirroredRepeatWrapping
+    doorTexture.wrapT = THREE.MirroredRepeatWrapping
+    doorTexture.offset.x = -0.2
+    doorTexture.offset.y = 0.5
+    doorTexture.rotation = Math.PI / 4
+
+    const minecraftTexture = useTexture(minecraftImage)
+    minecraftTexture.magFilter = THREE.NearestFilter
+
+    return (
+        <>
+            <mesh position={[-3, 0, 0]}>
+                <boxBufferGeometry args={[3, 3, 3]} />
+                <meshBasicMaterial map={doorTexture} />
+            </mesh>
+            <mesh position={[2, 0, 0]}>
+                <boxBufferGeometry args={[3, 3, 3]} />
+                <meshBasicMaterial map={minecraftTexture} />
+            </mesh>
+        </>
+    )
 }
 
 export default () => (
