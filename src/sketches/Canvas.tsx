3843097473924918e4fe562b8206f41538d6b9fc
“Isaac <isaac@isaacmason.com>
2022-09-15 00:41:34 +1000

    feat: add Suspsense with loader to sketch Canvas component

--- src/sketches/Canvas.tsx
+++ src/sketches/Canvas.tsx
@@ -1,7 +1,11 @@
 import { Canvas as R3FCanvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import { Loader } from '../Loader'
 
 export const Canvas = ({ children, ...rest }: Parameters<typeof R3FCanvas>[0]) => (
-    <R3FCanvas id="gl" {...rest}>
-        {children}
-    </R3FCanvas>
+    <Suspense fallback={<Loader />}>
+        <R3FCanvas id="gl" {...rest}>
+            {children}
+        </R3FCanvas>
+    </Suspense>
 )
