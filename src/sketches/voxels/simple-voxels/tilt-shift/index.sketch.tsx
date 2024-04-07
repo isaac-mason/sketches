991b3c924f67ebaecaefb756804e8500d794ecf2
“Isaac <isaac@isaacmason.com>
2024-04-08 09:28:47 +1000

    feat: simple voxels tilt shift sketch changes

--- src/sketches/voxels/simple-voxels/tilt-shift/index.sketch.tsx
+++ src/sketches/voxels/simple-voxels/tilt-shift/index.sketch.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@/common'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { EffectComposer, TiltShift2, ToneMapping } from '@react-three/postprocessing'
+import { EffectComposer, TiltShift, ToneMapping } from '@react-three/postprocessing'
 import { VoxelChunkMeshes, Voxels } from '../lib/react'
 import { useGoxelLevel } from '../use-goxel-level'
 import metropolisMapUrl from './metropolis.txt?url'
@@ -16,16 +16,20 @@ export default function Sketch() {
         <Canvas>
             <Voxels>
                 <VoxelMap />
+
+                <group onPointerDown={console.log}>
+
                 <VoxelChunkMeshes />
+                </group>
             </Voxels>
 
             <EffectComposer stencilBuffer enableNormalPass={false} autoClear={false} multisampling={4}>
-                <TiltShift2 samples={5} blur={0.1} />
+                <TiltShift focusArea={0.6} />
                 <ToneMapping />
             </EffectComposer>
 
             <PerspectiveCamera makeDefault fov={40} position={[300, 60, 200]} />
-            <OrbitControls makeDefault target={[50, -50, 0]} />
+            <OrbitControls makeDefault target={[78, -30, 36]} />
 
             <ambientLight intensity={1.5} />
 
