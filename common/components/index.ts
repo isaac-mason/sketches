41edb8f731255019bf1bd7b7033c424d61f9bab0
“Isaac <isaac@isaacmason.com>
2024-08-21 01:05:02 +1000

    fix: remove unused InteractionBarrier component

--- common/components/index.ts
+++ common/components/index.ts
@@ -1,7 +1,6 @@
 export * from './canvas'
 export * from './crosshair'
 export * from './instructions'
-export * from './interaction-barrier'
 export * from './spinner'
 export * from './third-person-controls'
 export * from './webgpu-canvas'
