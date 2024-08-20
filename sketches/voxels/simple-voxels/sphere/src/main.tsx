1acad0b42c47a9af4bddc43ea9e0663e30368523
“Isaac <isaac@isaacmason.com>
2024-08-21 00:58:17 +1000

    fix: remove console log

--- sketches/voxels/simple-voxels/sphere/src/main.tsx
+++ sketches/voxels/simple-voxels/sphere/src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Sketch } from './sketch'
 
-console.log('crossOriginIsolated', crossOriginIsolated)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Sketch />
