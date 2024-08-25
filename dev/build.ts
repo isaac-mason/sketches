a1b8befad6ec59fe863a048c2afae36797a1e7e2
“Isaac <isaac@isaacmason.com>
2024-08-25 12:47:06 +1000

    chore: refactor dev/build.ts

--- dev/build.ts
+++ dev/build.ts
@@ -9,29 +9,21 @@ import {
 } from './utils'
 
 console.log('creating sketches metadata')
-
 const sketchesMeta = await createSketchesMeta()
 
 console.log('writing sketches meta')
-
 writeSketchesMeta(sketchesMeta)
 
 console.log('building sketches')
-
 for (const { path } of sketchesMeta) {
     console.log(`\n\nbuilding ${path} ...\n`)
-
     await $`(cd sketches/${path} && yarn build)`.cwd(rootDirectory)
 }
 
 console.log('copying sketch builds to sketches-static')
-
 await $`rm -rf container/public/sketches-static`.cwd(rootDirectory)
-
 await copySketchBuilds(sketchesMeta)
-
 await copySketchCoverImages(sketchesMeta)
 
 console.log('building container')
-
 await $`yarn build`.cwd(containerAppDirectory)
