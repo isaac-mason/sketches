d65b9e92a5269b2b28fa53e5a66a6996da3166b0
“Isaac <isaac@isaacmason.com>
2024-09-04 11:30:37 +1000

    feat: minor logging changes in in build script

--- dev/build.ts
+++ dev/build.ts
@@ -8,22 +8,30 @@ import {
     writeSketchesMeta,
 } from './utils'
 
-console.log('creating sketches metadata')
-const sketchesMeta = await createSketchesMeta()
+try {
+    console.log('⏳ creating sketches metadata')
+    const sketchesMeta = await createSketchesMeta()
 
-console.log('writing sketches meta')
-writeSketchesMeta(sketchesMeta)
+    console.log('⏳ writing sketches meta')
+    writeSketchesMeta(sketchesMeta)
 
-console.log('building sketches')
-for (const { path } of sketchesMeta) {
-    console.log(`\n\nbuilding ${path} ...\n`)
-    await $`(cd sketches/${path} && yarn build)`.cwd(rootDirectory)
+    console.log('⏳ building sketches')
+    for (const { path } of sketchesMeta) {
+        console.log(`\n⏳ building sketch: ${path}\n`)
+        await $`(cd sketches/${path} && yarn build)`.cwd(rootDirectory)
+    }
+
+    console.log('⏳ copying sketch builds to sketches-static')
+    await $`rm -rf container/public/sketches-static`.cwd(rootDirectory)
+    await copySketchBuilds(sketchesMeta)
+    await copySketchCoverImages(sketchesMeta)
+
+    console.log('⏳ building container app\n')
+    await $`yarn build`.cwd(containerAppDirectory)
+
+    console.log('\n\n✅ build succeded')
+} catch (e) {
+    console.error('\n\n❌ build failed')
+    console.error(e)
+    process.exit(1)
 }
-
-console.log('copying sketch builds to sketches-static')
-await $`rm -rf container/public/sketches-static`.cwd(rootDirectory)
-await copySketchBuilds(sketchesMeta)
-await copySketchCoverImages(sketchesMeta)
-
-console.log('building container')
-await $`yarn build`.cwd(containerAppDirectory)
