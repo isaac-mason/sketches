25f1cac22f79bc4023ac8b75b541b84e34cb06d2
“Isaac <isaac@isaacmason.com>
2025-03-02 14:49:05 +1000

    feat: bump rapier revolute joint vehicle deps

--- common/components/instructions.tsx
+++ common/components/instructions.tsx
@@ -1,27 +1,22 @@
-import styled from 'styled-components'
-
-const InstructionsWrapper = styled.div`
-    color: white;
-    font-size: 1.2em;
-    left: 50px;
-    position: absolute;
-    bottom: 30px;
-    line-height: 1.5;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-
-    display: block;
-    font-family: monospace;
-    white-space: pre;
-
-    text-shadow: 1px 1px 1px black;
-`
-
 type InstructionsProps = {
     children: React.ReactNode
 }
 
+const INSTRUCTIONS_STYLES: React.CSSProperties = {
+    color: 'white',
+    fontSize: '1.2em',
+    left: '50px',
+    position: 'absolute',
+    bottom: '30px',
+    lineHeight: '1.5',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    fontFamily: 'monospace',
+    whiteSpace: 'pre',
+    textShadow: '1px 1px 1px black',
+}
+
 export const Instructions = ({ children }: InstructionsProps) => {
-    return <InstructionsWrapper>{children}</InstructionsWrapper>
+    return <div style={INSTRUCTIONS_STYLES}>{children}</div>
 }
