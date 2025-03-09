ad2b443c9c2dd0c32155a4bfcddd7df1d96ef99f
Isaac Mason <isaac@isaacmason.com>
2025-03-09 12:32:49 +1000

    fix: react 19 type fixes (#411)

--- common/hooks/use-const.ts
+++ common/hooks/use-const.ts
@@ -1,7 +1,7 @@
 import { useRef } from 'react'
 
 export const useConst = <T>(fn: () => T): T => {
-    const ref = useRef<T>()
+    const ref = useRef<T>(null!)
 
     if (!ref.current) {
         ref.current = fn()
